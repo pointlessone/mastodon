cf01eda08e992a8f812d28b528b2e01e91e968ab
Alexander Mankuta <alex@pointless.one>
2017-04-09 13:38:02 +0300

    Switch to my fork for deployment

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,8 @@
 lock '3.7.2'
 
 set :application, 'mastodon'
-set :repo_url, 'https://github.com/tootsuite/mastodon.git'
-set :branch, 'master'
+set :repo_url, 'https://github.com/pointlessone/mastodon.git'
+set :branch, 'pointlessone'
 gemset = File.read('.ruby-gemset').strip
 gemset = fetch(:application) if gemset.empty?
 set :rvm_ruby_version, "#{File.read('.ruby-version').strip}@#{gemset}"
